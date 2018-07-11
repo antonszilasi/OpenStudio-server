65b36131653c4e9909dad5a8a69215e52ee13c75
padmassun <padmassun@gmail.com>
2018-07-11 14:01:13 -0400

    added Download button

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -39,7 +39,10 @@ Rails.application.routes.draw do
         get :download_algorithm_results_zip
         get :dencity
         get :download_selected_datapoints
-
+        get :download_BTAP_results_zip
+        get :download_BTAP_error_log
+        get :download_BTAP_simulations
+        
         get :plot_xy_interactive
         post :plot_xy_interactive
       end
